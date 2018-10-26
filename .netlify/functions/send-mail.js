9a8a0815d475dddabbb95a098c518465415b40d1
Anand Graves <anand@voorhoede.nl>
2018-10-26 15:36:49 +0200

    refactor: simplified for testing

---
+++ .netlify/functions/send-mail.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv'
+import postmark from 'postmark'
+
+dotenv.config()
+
+const POSTMARK_SERVER_TOKEN = process.env.POSTMARK_SERVER_TOKEN
+const EMAIL_FROM = process.env.EMAIL_FROM
+const EMAIL_TO = process.env.EMAIL_TO
+const client = new postmark.Client(POSTMARK_SERVER_TOKEN)
+
+exports.handler = function (event, context, calback) {
+	client.sendEmail({
+		"From": EMAIL_FROM,
+		"To": EMAIL_TO,
+		"Subject": "Test",
+		"TextBody": "Hello from Postmark!"
+	})
+}
