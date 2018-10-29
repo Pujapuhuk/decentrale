b7c688b6c403b3e85e42de02f854df89b4da6ecb
Anand Graves <anand@voorhoede.nl>
2018-10-29 14:57:46 +0100

    refactor: changes for sending email via Postmark

--- src/server/functions/send-mail.js
+++ src/server/functions/send-mail.js
@@ -1,15 +1,40 @@
 const postmark = require('postmark')
-const POSTMARK_SERVER_TOKEN = "ff280493-677d-486a-a10a-7bf5246710d0"
-const EMAIL_FROM = "reserveer@decentraledelft.nl"
-const EMAIL_TO = "hallo@decentraledelft.nl"
+const dotenv = require('dotenv')
+const queryString = require('query-string')
+
+dotenv.config()
+
+const POSTMARK_SERVER_TOKEN = process.env.POSTMARK_SERVER_TOKEN
+const EMAIL_FROM = process.env.EMAIL_FROM
+const EMAIL_TO = process.env.EMAIL_TO
 const client = new postmark.Client(POSTMARK_SERVER_TOKEN)
 
+function getEmailBody(data) {
+	return Object.keys(data).reduce((order, next) => {
+		order += `${next}\n ${data[next]}\n\n`
+		return order
+	}, '')
+}
+
 exports.handler = function (event, context, callback) {
+	if (event.httpMethod !== 'POST' || !event.body) {
+		callback(null, {
+			statusCode: 405,
+			body: ''
+		});
+		return
+	}
+
+	const postData = queryString.parse(event.body)
+	const date = postData.date
+	const time = postData.time
+	const email = postData.email
+	const emailBody = getEmailBody(postData)
 	const options = {
 		"From": EMAIL_FROM,
 		"To": EMAIL_TO,
-		"Subject": "Test",
-		"TextBody": "Hello from Postmark!"
+		"Subject": `[${date}] - [${time}] Reservering De Centrale van ${email}`,
+		"TextBody": emailBody
 	}
 	const callbackHandler = {
 		statusCode: 302,
@@ -21,7 +46,6 @@ exports.handler = function (event, context, callback) {
 
 	client.sendEmail(options)
 		.then(result => {
-			console.log('result', result)
 			callback(null, callbackHandler)
 		})
 		.catch(error => console.error('error', error))
