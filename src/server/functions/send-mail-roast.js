7b5f3a2c84c63401d1f946d889bfadb5a537da01
Floor de Weijer <floordeweijer@gmail.com>
2020-10-16 16:37:20 +0200

    improvements

--- src/server/functions/send-mail-roast.js
+++ src/server/functions/send-mail-roast.js
@@ -48,7 +48,7 @@ exports.handler = function (event, context, callback) {
 		"From": `${name} <${EMAIL_FROM}>`,
 		"To": EMAIL_TO,
 		"ReplyTo": email,
-		"Subject": `${date} | ${time} | ${aantal}'personen' Roast bestelling bij De Centrale`,
+		"Subject": `${date} | ${time} | ${aantal} personen | Roast bestelling bij De Centrale`,
 		"TextBody": `
 		Datum: ${date}
 		Tijd: ${time}\n
