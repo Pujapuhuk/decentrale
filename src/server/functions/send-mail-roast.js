36aa0730a25b98250305d23fa72683a0ba8c2cec
FloordeWeijer <floordeweijer@gmail.com>
2021-03-26 20:20:14 +0100

    spekkoek eraf, roast uitverkocht

--- src/server/functions/send-mail-roast.js
+++ src/server/functions/send-mail-roast.js
@@ -40,7 +40,6 @@ exports.handler = function (event, context, callback) {
 	const phone = postData.phone
 	const aantal = postData.aantal
 	const cadeaubon = postData.cadeaubon
-	const spekkoek = postData.spekkoek
 	const nieuwsbrief = postData.nieuwsbrief
 	const emailBody = getEmailBody(postData)
 	const honeyPotValue = postData.petsName
@@ -61,7 +60,6 @@ exports.handler = function (event, context, callback) {
 		Datum: ${date}
 		Tijd: ${time}\n
 		Aantal: ${aantal} personen
-		Spekkoek: ${spekkoek} x
 		Bijpassende wijn: ${wijn}x \n
 		Extra:
 		Oester: ${oesters}
